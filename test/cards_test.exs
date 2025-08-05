bc338a4057906efef000992f55738d18a32184f6
yubinex <janhauck99@gmail.com>
2025-08-05 02:17:04 +0200

    test: Add tests for deck creation and shuffling
    This commit adds two new tests to `test/cards_test.exs`:
    
    - A test to ensure that `create_deck/0` creates a deck of 20 cards.
    - A test to verify that `shuffle/1` randomizes the order of the deck.

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,4 +1,17 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    # Set a fixed seed for deterministic shuffling
+    :rand.seed(:exsplus, {123, 456, 789})
+
+    deck = Cards.create_deck()
+    refute deck == Cards.shuffle(deck)
+  end
 end
